323208718e0f6b802a15b536ce1931a3a3ee6064
MauricioJeria <mauricio.tst15@gmail.com>
2024-10-19 20:16:47 -0300

    act. general v.5

--- src/app/paginas/qrgenerate/qrgenerate.component.ts
+++ src/app/paginas/qrgenerate/qrgenerate.component.ts
@@ -1,8 +1,8 @@
 import { IonicModule } from '@ionic/angular';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { HeaderComponent } from 'src/app/compartidos/header/header.component';
 import { FooterComponent } from 'src/app/compartidos/footer/footer.component';
-import { FormsModule } from '@angular/forms';
+import * as QRCode from 'qrcode';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -10,11 +10,36 @@ import { CommonModule } from '@angular/common';
   templateUrl: './qrgenerate.component.html',
   styleUrls: ['./qrgenerate.component.scss'],
   standalone: true,
-  imports: [IonicModule, HeaderComponent, FooterComponent,
-    FormsModule, CommonModule]
+  imports: [IonicModule, HeaderComponent, FooterComponent, CommonModule]
 })
-export class QrgenerateComponent   {
+export class QrgenerateComponent implements OnInit {
+  imagensrc: string = 'URL_QR_CODE_IMAGE_HERE';
+  fechaAct: string = '';
+  horaAct: string = '';
 
+  constructor() { }
 
+  ngOnInit(){
+    this.actualiFechaHora();
+    setInterval(() => {
+      this.actualiFechaHora();
+    }, 1000);
+  }
 
+  actualiFechaHora(){
+    const hoy = new Date();
+    this.fechaAct = hoy.toLocaleDateString();
+    this.horaAct = hoy.toLocaleTimeString();
+  }
+
+  rerollimage(){
+    const idrandom = Math.random().toString(36).substring(2,15);
+    const data = `registro-${idrandom}`;
+    QRCode.toDataURL(data), (err: Error | null, url: string) => {
+      if(err) {
+        return;
+  }
+  this.imagensrc = url;
+  }
+  }
 }
