1d6f24209b5f8c03904d01ca8679d67b6c3a85a0
MauricioJeria <mauricio.tst15@gmail.com>
2024-10-20 17:51:08 -0300

    act. general v.15

--- src/app/compartidos/footer/footer.component.ts
+++ src/app/compartidos/footer/footer.component.ts
@@ -1,13 +1,19 @@
+import { IonicModule } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   standalone: true,
+  imports: [IonicModule, CommonModule]
 })
 export class FooterComponent   {
 
 
 
 }
+
+
