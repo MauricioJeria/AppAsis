ecaa906084c5f51bbdd7da01e3797907df375087
MauricioJeria <mauricio.tst15@gmail.com>
2024-10-20 00:14:08 -0300

    act general v.10

--- src/app/paginas/qrscaner/qrscaner.component.ts
+++ src/app/paginas/qrscaner/qrscaner.component.ts
@@ -1,35 +1,79 @@
 import { IonicModule, AlertController } from '@ionic/angular';
-import { Component,  } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { HeaderComponent } from 'src/app/compartidos/header/header.component';
 import { FooterComponent } from 'src/app/compartidos/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { RouterModule } from '@angular/router';
 
 
+declare const navigator: any;
 
 @Component({
   selector: 'app-qrscaner',
   templateUrl: './qrscaner.component.html',
   styleUrls: ['./qrscaner.component.scss'],
   standalone: true,
-  imports: [IonicModule,HeaderComponent,FooterComponent, CommonModule]
+  imports: [IonicModule,HeaderComponent,FooterComponent,
+    CommonModule, ZXingScannerModule, RouterModule]
 })
-export class QrscanerComponent  {
+export class QrscanerComponent implements OnInit {
+  resultado: string | null = null;
+  mostrarScaner: boolean = false;
+  currentDevice: MediaDeviceInfo | null = null;
+
   constructor(private alertController: AlertController, private router: Router) { }
 
-  volver(){
+  async ngOnInit() {
+    try{
+      const stream = await navigator.mediaDevices.getUserMedia({video: true});
+      console.log('ACCESO CONSEDIDO', stream);
+      this.setupDevices();
+    }catch (err){
+      console.error('ERROR AL ACCEDER A LA CÁMARA', err);
+    }
+  }
+
+  async setupDevices() {
+
+    try {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter((device: MediaDeviceInfo) => device.kind === 'videoinput');
+
+      if (videoDevices.length > 0) {
+        this.currentDevice = videoDevices[0];
+        console.log('Cámara seleccionada:', this.currentDevice);
+      } else {
+        console.error('No se encontraron cámaras.');
+      }
+    } catch (err) {
+      console.error('Error al enumerar dispositivos:', err);
+    }
+  }
+
+  escanear(resultado: string){
+    this.resultado = resultado;
+    this.aqui();
     this.router.navigate(['/paginaestudiante']  );
   }
 
   async aqui(){
-    const alert = await this.alertController.create({
-      header: 'Alert',
-      message: 'Alumno Presente',
+    const alerta = await this.alertController.create({
+      header: 'alerta',
+      message: 'Alumno presente',
       buttons: ['OK']
     });
-    await alert.present();
+    await alerta.present();
+    }
+
+    controlescaneo(){
+      this.mostrarScaner = !this.mostrarScaner;
+    }
+
+
+  volver(){
+    this.router.navigate(['/paginaestudiante']  );
   }
-
-
 
 }
